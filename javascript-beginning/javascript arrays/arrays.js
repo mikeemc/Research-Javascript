b69f52b1bcf118deb65327032f81673708a3ff0b
Michael McHugh <12956611+mikeemc@users.noreply.github.com>
2020-08-23 16:23:11 +0200

    array updates

--- javascript-beginning/javascript arrays/arrays.js
+++ javascript-beginning/javascript arrays/arrays.js
@@ -75,3 +75,93 @@ const scienceStudentsCopy = [...scienceStudents];
 //  The original arrays remain unchanged
 mathStudentsCopy.pop();
 scienceStudentsCopy.push('Marvin');
+
+// Fetching a list using a for loop
+const students = ['Lee','Jim','Tina','Pete','Sharon'];
+for ( let i = 0; i < students.length; i++ ) { // Define Var, var less than Array amount, increment var
+  console.log(students[i]); // Log array value incrementally
+}
+
+// Get the average of an array of scores
+// a for loop accesses each number in the scores array and adds it to the current value of total. When the loop completes, the console displays the average score by dividing the value of total by the length of the scores array.
+const scores = [ 20, 50, 75, 100, 115 ];
+let total = 0;
+
+for ( let i = 0; i < scores.length; i++ ) {
+  total += scores[i];
+}
+
+console.log( total / scores.length );
+
+// Another example
+const playlist = [
+  'So What',
+  'Respect',
+  'What a Wonderful World',
+  'At Last',
+  'Three Little Birds',
+  'The Way You Look Tonight'
+];
+
+function createListItems(arr) { // The arr can match any parameter that need to be used
+  let items = '';
+  for (let i = 0; i < arr.length; i++) {
+    items += `<li>${arr[i]}</li>`;
+  }
+  return items; // return what is in the items var
+}
+
+document.querySelector('main').innerHTML = `
+  <ol>
+    ${createListItems(playlist)}// Use the variable as the parameter, then in the function, this array is being used.
+  </ol>
+`;
+
+// Useful Array methods (Join, Include, indexOf)
+const daysInWeek = [
+  'Monday',
+  'Tuesday',
+  'Wednesday'
+];
+// Join - arr.join() - daysInWeek.join(', '); - console.log(daysInWeek.join(', '));
+// Shows 'Monday, Tuesday, Wednesday'
+console.log(daysInWeek.join(', '));
+
+// Include - arr.includes('') - daysInWeek.includes('Monday'); - console.log(daysInWeek.includes('Monday'));
+// Shows boolean (True/False) - it is case sensitive
+console.log(daysInWeek.includes('Monday'));
+
+// Join - arr.indexOf() - daysInWeek.indexOf('Monday'); - console.log(daysInWeek.indexOf('Monday'));
+// Shows a numeric value 0
+// If the value isn't there, it returns -1 when it can't find a index of an element
+console.log(daysInWeek.indexOf('Monday'));
+
+// Another example - prompt pop up with if else
+const inStock = ['pizza', 'cookies', 'eggs', 'apples', 'milk', 'cheese', 'bread', 'lettuce', 'carrots', 'broccoli', 'potatoes', 'crackers', 'onions', 'tofu', 'limes', 'cucumbers'];
+const search = prompt('Search for a product.');
+let message;
+
+if (inStock.includes(search)) {
+  message = `Yes, we have <b>${search}</b>.`;
+} else {
+  message = `Sorry, we do not have <b>${search}</b>.`;
+}
+
+document.querySelector('main').innerHTML = `<p>${message}</p>`;
+
+// Locate and Join Array Elements
+const inStock = ['pizza', 'cookies', 'eggs', 'apples', 'milk', 'cheese', 'bread', 'lettuce', 'carrots', 'broccoli', 'potatoes', 'crackers', 'onions', 'tofu', 'limes', 'cucumbers'];
+const search = prompt('Search for a product.');
+let message;
+
+if (!search){ // if the user presses cancels or submits a blank search then this is used
+  message = `<b>In stock:</b> ${inStock.join(', ')}`;
+} else if (inStock.includes(search.toLowerCase())) { // Takes the array and uses the 'search' variable as a parameter - makes the input lowercase too
+  message = `Yes, we have <b>${search}</b>.`;
+} else {
+  message = `Sorry, we do not have <b>${search}</b>. It's #${inStock.indexOf(search.toLowerCase())+1} on the list!`; // Searches for the index of the search parameter adding 1
+}
+
+document.querySelector('main').innerHTML = `<p>${message}</p>`;
+
+// What is a Multidimensional Array?
